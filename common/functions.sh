6519361611ae5efaea6d191d724ab85cac901709
Briclyaz <prorok.new@mail.ru>
2021-05-05 12:31:55 +0300

    Change version on 2.5 BETA

--- common/functions.sh
+++ common/functions.sh
@@ -109,7 +109,7 @@ prop_process() {
   ui_print " "
   ui_print "***************************************************"
   ui_print "*                                                 *"
-  ui_print "*               NLSound v2.4 STABLE               *"
+  ui_print "*                NLSound v2.5 BETA                *"
   ui_print "*                                                 *"
   ui_print "*               special version for               *"
   ui_print "*                                                 *"
