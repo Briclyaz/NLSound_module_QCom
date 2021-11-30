5be5a5cb5fd64db1a7ba788f7a408a774d115d1e
Briclyaz <prorok.new@mail.ru>
2021-11-30 22:16:00 +0300

    Deleted HIGH PERF
    And fixed bug freezes during device reboot

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,12 +1,1 @@
 #!/system/bin/sh
-
-POST=$MODPATH/post-fs-data
-
-FORCING=$(find /sys/module -name high_perf_mode)
-
-chmod 0777 $POST
-
-busybox echo "1" > $FORCING
-
-setprop mic.volume 7
-
--- service.sh
+++ service.sh
@@ -1,15 +1,1 @@
 #!/system/bin/sh
-#author: akirasuper@github
-MOUNT=/data
-MODDIR=${0%/*}
-# Check if file exist
-FILE1=$(find /sys/module -name high_perf_mode)
-FILE2=$(find /sys/module -name cpe_debug_mode)
-FILE3=$(find /sys/module -name impedance_detect_en)
-# High Perf Mode
-echo 1 > $FILE1
-# CPE Debug Mode
-echo 1 > $FILE2
-# Impedance Detect EN
-echo 1 > $FILE3
-fi
